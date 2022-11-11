209c3b0f73f9646cad1a3c1eb6b9d1d5b992dedb
Star1215 <cryptomanagerstar@gmail.com>
2022-11-11 21:31:50 +0100

    ëvmos sdk v2

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0xd592B3a0CB5C262c14250a21dD121451b2187307";
+export declare const INIT_CODE_HASH = "0x9ff3fa50793cbf519adb1e1594682d293c972e4d877f26c72191c7d6ffda1f92";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
